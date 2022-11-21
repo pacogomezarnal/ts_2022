41f7133bce51c471b24f449fa00943f701484728
pacogomezarnal <paco.gomez.arnal@gmail.com>
2022-11-21 12:09:20 +0100

    web_server

--- cap2/src/index.js
+++ cap2/src/index.js
@@ -1,10 +1,12 @@
 const pedido=[];
+const carrito=document.getElementById('carrito');
 
 function log(texto){
     console.log(texto);
 }
 
 function nuevoElm(elm){
+    carrito.innerHTML=carrito.innerHTML+"<p>"+"Nigiri salmon"+"</p>"
     pedido.push(elm);
     log("[NUEVO] "+elm)
 }
